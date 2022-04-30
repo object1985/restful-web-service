15908d1b636db7923d91f46b67c6c4c765cb2d86
tonaise <object1985@gmail.com>
2022-04-30 10:05:25 +0900

    [addD]Step 18 - Part 2 - Internationalization.

--- src/main/java/jp/co/tonaise/rest/webservice/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/jp/co/tonaise/rest/webservice/restfulwebservice/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package jp.co.tonaise.rest.webservice.restfulwebservice.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,6 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world-internationalized")
     public String helloWorldInternationalized() {
-	return "Good Morning";
-//	return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+	return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
