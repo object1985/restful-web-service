42316238d375d897d4532e44de34046704356fcb
tonaise <object1985@gmail.com>
2022-05-11 17:29:48 +0900

    [add] Step 27 - Versioning RESTful Services - Header and Content Negotiation Approach

---
+++ src/main/java/jp/co/tonaise/rest/webservice/restfulwebservice/versioning/PersonV1.java
@@ -0,0 +1,18 @@
+package jp.co.tonaise.rest.webservice.restfulwebservice.versioning;
+
+public class PersonV1 {
+    private String name;
+
+    public PersonV1(String name) {
+	super();
+	this.name = name;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+}
